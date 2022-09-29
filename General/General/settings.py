ff6998cd688c478755a6bfd42f42bd968d817750
juanacevedo68061 <juan.acevedo68061@fpuna.edu.py>
2022-09-29 12:54:16 -0400

    agregando CSRF_TRUSTED_ORIGINS

--- General/General/settings.py
+++ General/General/settings.py
@@ -162,4 +162,5 @@ LOGOUT_REDIRECT_URL = '/'
 STATIC_ROOT = '/static/'
 STATIC_URL = '/static/'
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost:8080","http://localhost:8000","http://localhost:80"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:8080","http://localhost:8000","http://localhost:80",
+"http://127.0.0.1:80","http://127.0.0.1:8080","http://127.0.0.1:8000"]
